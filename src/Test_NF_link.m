22de09db5fdee0bc4e23d83cee4320a3d40d8812
FourierMax <13260129659@163.com>
2025-10-29 18:38:55 +0800

    update: license

--- src/Test_NF_link.m
+++ src/Test_NF_link.m
@@ -1,13 +1,13 @@
 % Description: This code implements a Topology-desynchronized Forwarding via Symmetry in Toroidal Networks.
 % It simulates packet transmission, link fault handling, and routing in a torus network topology, with statistical analysis of performance metrics.
-% Author: []
-% Affiliation: []
+% Author: [Shenshen Luan]
+% Affiliation: [Beihang University]
 % Date: [2025/10/10]
 % License: MIT License
 % 
 % MIT License
 % 
-% Copyright (c) [2025] [Name/Affiliation]
+% Copyright (c) [2025] [Shenshen Luan/Beihang University]
 % 
 % Permission is hereby granted, free of charge, to any person obtaining a copy
 % of this software and associated documentation files (the "Software"), to deal
